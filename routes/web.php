80d690032837b48e56a8ce0727476986bf8de8e2
dueityou <dueityou@gmail.com>
2025-11-28 15:13:18 +0900

    feat: PostController@indexへのルーティングの定義

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// /postにGETリクエストが送信されると、Postコントローラーのindexメソッドが実行される
+Route::get('/post', [PostController::class, 'index']);
