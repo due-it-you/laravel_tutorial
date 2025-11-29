f298665640e699113f4e08238aaefe69f3eb71ab
dueityou <dueityou@gmail.com>
2025-11-29 17:11:01 +0900

    feat: routeメソッドで投稿UIをリンク化

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,9 +7,12 @@
   <div>
     <!-- コントローラーから受け取った変数を繰り返し処理で展開 -->
     @foreach ($posts as $post)
-      <div class="border">
-        {{ $post->name }}
-      </div>
+      <!-- 第１引数に『名前付きルート』 第2引数に『パラメータ』を渡してURLを生成してくれる -->
+      <a href="{{ route('posts.show', $post) }}">
+        <div class="border hover:bg-gray-300">
+          {{ $post->name }}
+        </div>
+      </a>
     @endforeach
   </div>
 @endsection
