cfe06b7c51de3c3a6633b341a8772f0fbeaf65d4
dueityou <dueityou@gmail.com>
2025-12-07 13:09:55 +0900

    feat: UserモデルとPostモデルの1:多の関連付けの定義

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -10,4 +11,9 @@ class Post extends Model
     protected $fillable = [
         'name'
     ];
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
